01f750a695c684ec4011704dc2955fe1d8bc9877
encsi2346 <encsi2346@gmail.com>
2024-05-28 19:08:45 +0200

    user completion

--- transport_logistics_web_application_server/controllers/users.js
+++ transport_logistics_web_application_server/controllers/users.js
@@ -1,5 +1,4 @@
 import User from "../models/User.js";
-import Car from "../models/Car.js";
 
 export const getAllUsers = async (req, res) => {
     try {
@@ -20,88 +19,138 @@ export const getUser = async (req, res) => {
     }
 }
 
-
-export const createCar = async (req, res) => {
+export const createUser = async (req, res) => {
     console.log('req', req);
     try {
         const {
-            carId,
-            name,
-            type,
-            licencePlate,
-            numberOfRegistrationLicence,
-            chassisNumber,
-            yearOfProduction,
-            dateOfFirstRegistration,
-            images,
-            dateOfDatabaseRegistration,
-            dateOfLastTechnicalExamination,
-            dateOfLastService,
-            totalDrivenKm,
-            totalTransport,
+            userId,
+            firstName,
+            familyName,
+            email,
+            password,
+            picturePath,
+            gender,
+            nationality,
+            birthPlace,
+            birthDate,
+            IDCardNumber,
+            validityDateOfIDCard,
+            drivingLicenceNumber,
+            drivingLicenceCategories,
+            validityDateOfDrivingLicence,
+            dateOfMedicalVisit,
+            validityDateOfMedicalVisit,
+            phoneNumber,
+            country,
+            postcode,
+            city,
+            nameOfPublicArea,
+            typeOfPublicArea,
+            houseNumber,
+            dateOfRegistration,
+            startDateOfContract,
+            endDateOfContract,
+            position,
+            lineManager,
+            healthProblem,
+            createdAt,
         } = req.body;
-        const newCar = new Car({
-            carId,
-            name,
-            type,
-            licencePlate,
-            numberOfRegistrationLicence,
-            chassisNumber,
-            yearOfProduction,
-            dateOfFirstRegistration,
-            images,
-            dateOfDatabaseRegistration,
-            dateOfLastTechnicalExamination,
-            dateOfLastService,
-            totalDrivenKm,
-            totalTransport,
+        const newUser = new User({
+            userId,
+            firstName,
+            familyName,
+            email,
+            password,
+            picturePath,
+            gender,
+            nationality,
+            birthPlace,
+            birthDate,
+            IDCardNumber,
+            validityDateOfIDCard,
+            drivingLicenceNumber,
+            drivingLicenceCategories,
+            validityDateOfDrivingLicence,
+            dateOfMedicalVisit,
+            validityDateOfMedicalVisit,
+            phoneNumber,
+            country,
+            postcode,
+            city,
+            nameOfPublicArea,
+            typeOfPublicArea,
+            houseNumber,
+            dateOfRegistration,
+            startDateOfContract,
+            endDateOfContract,
+            position,
+            lineManager,
+            healthProblem,
+            createdAt,
         });
-        const savedCar = await newCar.save();
-        res.status(201).json(savedCar);
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 };
 
-export const updateCar = async (req, res) => {
+export const updateUser = async (req, res) => {
     try {
         const { id } = req.params;
-        const car = await Car.findById(id);
-        if (!car) {
-            return res.status(404).json({ message: 'Car not found' });
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
         }
-        if (req.body.carId) {
-            car.carId = req.body.carId;
-            car.name = req.body.name;
-            car.type = req.body.type;
-            car.licencePlate = req.body.licencePlate;
-            car.numberOfRegistrationLicence = req.body.numberOfRegistrationLicence;
-            car.chassisNumber = req.body.chassisNumber;
-            car.yearOfProduction = req.body.yearOfProduction;
-            car.dateOfFirstRegistration = req.body.dateOfFirstRegistration;
-            car.images = req.body.images;
-            car.dateOfDatabaseRegistration = req.body.dateOfDatabaseRegistration;
-            car.dateOfLastTechnicalExamination = req.body.dateOfLastTechnicalExamination;
-            car.dateOfLastService = req.body.dateOfLastService;
-            car.totalDrivenKm = req.body.totalDrivenKm;
-            car.totalTransport = req.body.totalTransport;
+        if (req.body.userId) {
+            user.userId = req.body.userId;
+            user.firstName = req.body.firstName;
+            user.familyName = req.body.familyName;
+            user.email = req.body.email;
+            user.password = req.body.password;
+            user.picturePath = req.body.picturePath;
+            user.gender = req.body.gender;
+            user.nationality = req.body.nationality;
+            user.birthPlace = req.body.birthPlace;
+            user.birthDate = req.body.birthDate;
+            user.IDCardNumber = req.body.IDCardNumber;
+            user.validityDateOfIDCard = req.body.validityDateOfIDCard;
+            user.drivingLicenceNumber = req.body.drivingLicenceNumber;
+            user.drivingLicenceCategories = req.body.drivingLicenceCategories;
+            user.validityDateOfDrivingLicence = req.body.validityDateOfDrivingLicence;
+            user.dateOfMedicalVisit = req.body.dateOfMedicalVisit;
+            user.validityDateOfMedicalVisit = req.body.validityDateOfMedicalVisit;
+            user.phoneNumber = req.body.phoneNumber;
+            user.country = req.body.country;
+            user.postcode = req.body.postcode;
+            user.city = req.body.city;
+            user.nameOfPublicArea = req.body.nameOfPublicArea;
+            user.typeOfPublicArea = req.body.typeOfPublicArea;
+            user.houseNumber = req.body.houseNumber;
+            user.dateOfRegistration = req.body.dateOfRegistration;
+            user.startDateOfContract = req.body.startDateOfContract;
+            user.endDateOfContract = req.body.endDateOfContract;
+            user.position = req.body.position;
+            user.lineManager = req.body.lineManager;
+            user.healthProblem = req.body.healthProblem;
+            user.createdAt = req.body.createdAt;
         }
-        const updatedCar = await car.save();
-        res.json(updatedCar);
+        const updatedUser = await user.save();
+        res.json(updatedUser);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 };
 
-export const deleteCar = async (req, res) => {
+export const deleteUser = async (req, res) => {
     try {
         const { id } = req.params;
-        const car = await Car.findById(id);
-        if (!car) {
-            return res.status(404).json({ message: 'Car not found' });
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
         }
-        await car.deleteOne();
-        res.json({ message: 'Car deleted' });
+        await user.deleteOne();
+        res.json({ message: 'User deleted' });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
--- transport_logistics_web_application_server/routes/users.js
+++ transport_logistics_web_application_server/routes/users.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {
+    createUser, deleteUser,
     getAllUsers,
-    getUser,
+    getUser, updateUser,
 } from "../controllers/users.js";
 //import { verifyToken} from "../middleware/auth.js";
 
@@ -52,5 +53,87 @@ router.get("/api/users/:id", getUser);
  *                              $ref: '#components/schemas/User'
  */
 router.get('/api/users', getAllUsers);
+
+
+/**
+ * @swagger
+ * /api/users/addUser:
+ *   post:
+ *     summary: Add a new user.
+ *     tags: [Users]
+ *     description: Add a new user to the database.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       201:
+ *         description: The newly created user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad request.
+ */
+router.post('/api/users/addUser', createUser);
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   put:
+ *     summary: Update a user.
+ *     tags: [Users]
+ *     description: Update a user in the database.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the user to update.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The updated user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad request.
+ *       404:
+ *         description: User not found.
+ */
+router.put('/api/users/:id', updateUser);
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     summary: Delete a user.
+ *     tags: [Users]
+ *     description: Delete a user from the database.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the user to delete.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: User deleted successfully.
+ *       404:
+ *         description: User not found.
+ */
+router.delete('/api/users/:id', deleteUser);
 
 export default router;
